38a19f0b349ceefd8afc5df72d0ce84f5b19d2da
huangdinghua <http://gitlab.shidaoffice.vip/>
2022-02-24 12:28:05 +0800

    jitpack.io

--- app/src/main/java/com/huarrrr/floatview_widget/MainActivity.kt
+++ app/src/main/java/com/huarrrr/floatview_widget/MainActivity.kt
@@ -2,14 +2,16 @@ package com.huarrrr.floatview_widget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import com.huarrrr.floatview.FloatingViewKotlin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        FloatingViewKotlin(this, onClickAction = {
+        }).apply {
+            showFloat()
+        }
 
 //        FloatingViewJava floatView = FloatingViewJava(this) {
 //            //点击
